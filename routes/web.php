da43e3e00df379daa30d45938e37546476e4aaf6
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-21 15:36:57 +0600

    Adding Multi Auth System with Breeze (part-1)

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('admin.dashboard.index');
+    return view('welcome');
 });
 
 Route::get('/dashboard', function () {
