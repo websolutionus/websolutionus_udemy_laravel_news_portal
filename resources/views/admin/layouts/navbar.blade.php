d38b8a0431c9c57fe49a7f64d6417d6da25bf4d5
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-21 15:15:38 +0600

    Admin Layout Mastering Dashboard Page (part-2)

---
+++ resources/views/admin/layouts/navbar.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg main-navbar">
+
+    <ul class="navbar-nav navbar-right ml-auto">
+
+        <li class="dropdown"><a href="#" data-toggle="dropdown"
+                class="nav-link dropdown-toggle nav-link-lg nav-link-user">
+                <img alt="image" src="assets/img/avatar/avatar-1.png" class="rounded-circle mr-1">
+                <div class="d-sm-none d-lg-inline-block">Hi, Ujang Maman</div>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right">
+                <div class="dropdown-title">Logged in 5 min ago</div>
+                <a href="features-profile.html" class="dropdown-item has-icon">
+                    <i class="far fa-user"></i> Profile
+                </a>
+                <a href="features-activities.html" class="dropdown-item has-icon">
+                    <i class="fas fa-bolt"></i> Activities
+                </a>
+                <a href="features-settings.html" class="dropdown-item has-icon">
+                    <i class="fas fa-cog"></i> Settings
+                </a>
+                <div class="dropdown-divider"></div>
+                <a href="#" class="dropdown-item has-icon text-danger">
+                    <i class="fas fa-sign-out-alt"></i> Logout
+                </a>
+            </div>
+        </li>
+    </ul>
+</nav>
