adee1ae13a4955d6748a965f37ac6c2c3ed127f9
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-28 14:59:51 +0600

    News Post - Handling Create From Data ( Part - 3)

--- app/Http/Controllers/Admin/NewsController.php
+++ app/Http/Controllers/Admin/NewsController.php
@@ -6,10 +6,15 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\AdminNewsCreateRequest;
 use App\Models\Category;
 use App\Models\Language;
+use App\Models\News;
+use App\Traits\FileUploadTrait;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class NewsController extends Controller
 {
+    use FileUploadTrait;
+
     /**
      * Display a listing of the resource.
      */
@@ -41,8 +46,29 @@ class NewsController extends Controller
      */
     public function store(AdminNewsCreateRequest $request)
     {
+        /** Handle image */
+        $imagePath = $this->handleFileUpload($request, 'image');
 
-        dd($request->all());
+        $news = new News();
+        $news->language = $request->language;
+        $news->category_id = $request->category;
+        $news->auther_id = Auth::guard('admin')->user()->id;
+        $news->image = $imagePath;
+        $news->title = $request->title;
+        $news->slug = \Str::slug($request->title);
+        $news->content = $request->content;
+        $news->meta_title = $request->meta_title;
+        $news->meta_description = $request->meta_description;
+        $news->is_breaking_news = $request->is_breaking_news == 1 ? 1 : 0;
+        $news->show_at_slider = $request->show_at_slider == 1 ? 1 : 0;
+        $news->show_at_popular = $request->show_at_popular == 1 ? 1 : 0;
+        $news->status = $request->status == 1 ? 1 : 0;
+        $news->save();
+
+        toast(__('Created Successfully!'), 'success')->width('130');
+
+        return redirect()->route('admin.news.index');
+
     }
 
     /**
