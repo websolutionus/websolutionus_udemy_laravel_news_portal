1a2cc29e64596c47e7cded66075ecefacbacce75
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-28 18:18:43 +0600

    News Post - Showing Created Data at Index Page

--- app/Models/News.php
+++ app/Models/News.php
@@ -9,6 +9,11 @@ class News extends Model
 {
     use HasFactory;
 
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
     public function tags()
     {
         return $this->belongsToMany(Tag::class, 'news_tags');
--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -28,9 +28,11 @@
 
                 </ul>
                 <div class="tab-content tab-bordered" id="myTab3Content">
-                    {{-- @foreach ($languages as $language)
+                    @foreach ($languages as $language)
                         @php
-                            $categories = \App\Models\Category::where('language', $language->lang)->orderByDesc('id')->get();
+                            $news = \App\Models\News::with('category')
+                                ->where('language', $language->lang)
+                                ->orderBy('id', 'DESC')->get();
                         @endphp
                         <div class="tab-pane fade show {{ $loop->index === 0 ? 'active' : '' }}"
                             id="home-{{ $language->lang }}" role="tabpanel" aria-labelledby="home-tab2">
@@ -42,41 +44,60 @@
                                                 <th class="text-center">
                                                     #
                                                 </th>
-                                                <th>{{ __('Name') }}</th>
-                                                <th>{{ __('Language Code') }}</th>
-                                                <th>{{ __('In Nav') }}</th>
+                                                <th>{{ __('Image') }}</th>
+                                                <th>{{ __('Title') }}</th>
+                                                <th>{{ __('Category') }}</th>
+                                                <th>{{ __('In Breaking') }}</th>
+                                                <th>{{ __('In Slider') }}</th>
+                                                <th>{{ __('In Popular') }}</th>
                                                 <th>{{ __('Status') }}</th>
                                                 <th>{{ __('Action') }}</th>
                                             </tr>
                                         </thead>
                                         <tbody>
-                                            @foreach ($categories as $category)
+                                            @foreach ($news as $item)
                                                 <tr>
-                                                    <td>{{ $category->id }}</td>
-                                                    <td>{{ $category->name }}</td>
-                                                    <td>{{ $category->language }}</td>
+                                                    <td>{{ $item->id }}</td>
                                                     <td>
-                                                        @if ($category->show_at_nav == 1)
-                                                            <span class="badge badge-primary">{{ __('Yes') }}</span>
-                                                        @else
-                                                            <span class="badge badge-danger">{{ __('No') }}</span>
-                                                        @endif
-
-                                                    </td>
-                                                    <td>
-                                                        @if ($category->status == 1)
-                                                            <span class="badge badge-success">{{ __('Yes') }}</span>
-                                                        @else
-                                                            <span class="badge badge-danger">{{ __('No') }}</span>
-                                                        @endif
-
+                                                        <img src="{{ asset($item->image) }}" width="100" alt="">
                                                     </td>
 
+                                                    <td>{{ $item->title }}</td>
+                                                    <td>{{ $item->category->name }}</td>
 
                                                     <td>
-                                                        <a href="{{ route('admin.category.edit', $category->id) }}" class="btn btn-primary"><i
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->is_breaking_news === 1 ? 'checked' : '' }} value="1" type="checkbox" class="custom-switch-input">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_slider === 1 ? 'checked' : '' }} value="1" type="checkbox" class="custom-switch-input">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->show_at_popular === 1 ? 'checked' : '' }} value="1" type="checkbox" class="custom-switch-input">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+                                                    <td>
+                                                        <label class="custom-switch mt-2">
+                                                            <input {{ $item->status === 1 ? 'checked' : '' }} value="1" type="checkbox" class="custom-switch-input">
+                                                            <span class="custom-switch-indicator"></span>
+                                                        </label>
+                                                    </td>
+
+
+                                                    <td>
+                                                        <a href="{{ route('admin.category.edit', $item->id) }}" class="btn btn-primary"><i
                                                                 class="fas fa-edit"></i></a>
-                                                        <a href="{{ route('admin.category.destroy', $category->id) }}" class="btn btn-danger delete-item"><i
+                                                        <a href="{{ route('admin.category.destroy', $item->id) }}" class="btn btn-danger delete-item"><i
                                                                 class="fas fa-trash-alt"></i></a>
                                                     </td>
                                                 </tr>
@@ -88,7 +109,7 @@
                                 </div>
                             </div>
                         </div>
-                    @endforeach --}}
+                    @endforeach
 
                 </div>
             </div>
@@ -105,7 +126,8 @@
                 "columnDefs": [{
                     "sortable": false,
                     "targets": [2, 3]
-                }]
+                }],
+                "order": [[0, 'desc']]
             });
         @endforeach
     </script>
