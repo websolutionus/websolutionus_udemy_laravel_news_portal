f95065303f06b9431a705d27da263adf30cc5a6c
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-28 10:24:43 +0600

    News Post - Creating Necessary Pages and Designs

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -17,9 +17,22 @@
                 <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
             </li>
             <li class="menu-header">Starter</li>
-            <li><a class="nav-link" href="{{ route('admin.category.index') }}"><i class="far fa-square"></i> <span>Category</span></a></li>
+            <li><a class="nav-link" href="{{ route('admin.category.index') }}"><i class="far fa-square"></i>
+                    <span>Category</span></a></li>
 
-            <li><a class="nav-link" href="{{ route('admin.language.index') }}"><i class="far fa-square"></i> <span>Languages</span></a></li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>News</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="{{ route('admin.news.index') }}">All News</a></li>
+                    <li><a class="nav-link" href="forms-editor.html">Editor</a></li>
+                    <li><a class="nav-link" href="forms-validation.html">Validation</a></li>
+                </ul>
+            </li>
+
+            <li><a class="nav-link" href="{{ route('admin.language.index') }}"><i class="far fa-square"></i>
+                    <span>Languages</span></a></li>
+
+
 
             {{-- <li><a class="nav-link" href="blank.html"><i class="far fa-square"></i> <span>Blank Page</span></a></li> --}}
 
--- routes/admin.php
+++ routes/admin.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\Admin\AdminAuthenticationController;
 use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LanguageController;
+use App\Http\Controllers\Admin\NewsController;
 use App\Http\Controllers\Admin\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -35,6 +36,9 @@ Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => ['admin']],
 
     /** Category Route */
     Route::resource('category', CategoryController::class);
+
+    /** Category Route */
+    Route::resource('news', NewsController::class);
 
 
 });
