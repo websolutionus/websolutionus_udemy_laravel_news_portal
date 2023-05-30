9cf76b6c3061957f89007fede7fbd6a8956a4995
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-30 11:50:55 +0600

    Set Up Language - Get Language Code From Session

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,8 +1,35 @@
 <?php
 
+use App\Models\Language;
+
 /** format news tags */
 
 function formatTags(array $tags): String
 {
    return implode(',', $tags);
+}
+
+/** get selected language from session */
+function getLangauge(): string
+{
+    if(session()->has('language')){
+        return session('language');
+    }else {
+        try {
+            $language = Language::where('default', 1)->first();
+            setLanguage($language->lang);
+
+            return $language->lang;
+        } catch (\Throwable $th) {
+            setLanguage('en');
+
+            return $language->lang;
+        }
+    }
+}
+
+/** set language code in session */
+function setLanguage(string $code): void
+{
+    session(['language' => $code]);
 }
