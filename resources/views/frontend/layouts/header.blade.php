81383e9912dcec6cd73956347f25fad6087e0358
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-05-30 13:30:16 +0600

    Set Up Language - Make Selected Language Input Depending on Session

--- resources/views/frontend/layouts/header.blade.php
+++ resources/views/frontend/layouts/header.blade.php
@@ -31,7 +31,7 @@
                         <div class="topbar_language">
                             <select id="site-language">
                                 @foreach ($languages as $language)
-                                    <option value="{{ $language->lang }}" {{ $language->default === 1 ? 'selected' : '' }}>{{ $language->name }}</option>
+                                    <option value="{{ $language->lang }}" {{ getLangauge() === $language->lang ? 'selected' : '' }}>{{ $language->name }}</option>
                                 @endforeach
                             </select>
                         </div>
