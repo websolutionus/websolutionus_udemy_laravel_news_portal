9f3069145450eaf082c0aa89a8954f7ab280229f
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-06 12:38:24 +0600

    News Letter - Storing Subscribers Email

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -9,6 +9,7 @@ use App\Models\Comment;
 use App\Models\HomeSectionSetting;
 use App\Models\News;
 use App\Models\SocialCount;
+use App\Models\Subscriber;
 use App\Models\Tag;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -242,8 +243,16 @@ class HomeController extends Controller
     public function SubscribeNewsLetter(Request $request)
     {
        $request->validate([
-        'email' => ['required', 'email', 'max:255']
+        'email' => ['required', 'email', 'max:255', 'unique:subscribers,email']
+       ],[
+        'email.unique' => __('Email is already subscribed!')
        ]);
-       
+
+       $subscriber = new Subscriber();
+       $subscriber->email = $request->email;
+       $subscriber->save();
+
+       return response(['status' => 'success', 'message' => __('Subscribed successfully!')]);
+
     }
 }
--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -6,13 +6,13 @@
     <title>@yield('title')</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
 
-    <meta name="description" content="@yield('meta_description')"/>
-    <meta name="og:title" content="@yield('meta_og_title')"/>
-    <meta name="og:description" content="@yield('meta_og_description')"/>
-    <meta name="og:image" content="@yield('meta_og_image')"/>
-    <meta name="twitter:title" content="@yield('meta_tw_title')"/>
-    <meta name="twitter:description" content="@yield('meta_tw_description')"/>
-    <meta name="twitter:image" content="@yield('meta_tw_image')"/>
+    <meta name="description" content="@yield('meta_description')" />
+    <meta name="og:title" content="@yield('meta_og_title')" />
+    <meta name="og:description" content="@yield('meta_og_description')" />
+    <meta name="og:image" content="@yield('meta_og_image')" />
+    <meta name="twitter:title" content="@yield('meta_tw_title')" />
+    <meta name="twitter:description" content="@yield('meta_tw_description')" />
+    <meta name="twitter:image" content="@yield('meta_tw_image')" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="{{ asset('frontend/assets/css/styles.css') }}" rel="stylesheet">
@@ -39,18 +39,17 @@
     <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 
     <script>
-
         const Toast = Swal.mixin({
-                toast: true,
-                position: 'top-end',
-                showConfirmButton: false,
-                timer: 3000,
-                timerProgressBar: true,
-                didOpen: (toast) => {
-                    toast.addEventListener('mouseenter', Swal.stopTimer)
-                    toast.addEventListener('mouseleave', Swal.resumeTimer)
-                }
-            })
+            toast: true,
+            position: 'top-end',
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.addEventListener('mouseenter', Swal.stopTimer)
+                toast.addEventListener('mouseleave', Swal.resumeTimer)
+            }
+        })
 
 
         // Add csrf token in ajax request
@@ -60,46 +59,57 @@
             }
         });
 
-        $(document).ready(function(){
+        $(document).ready(function() {
             /** change language **/
-            $('#site-language').on('change', function(){
+            $('#site-language').on('change', function() {
                 let languageCode = $(this).val();
                 $.ajax({
                     method: 'GET',
                     url: "{{ route('language') }}",
-                    data: {language_code: languageCode},
-                    success: function(data){
-                        if(data.status === 'success'){
+                    data: {
+                        language_code: languageCode
+                    },
+                    success: function(data) {
+                        if (data.status === 'success') {
                             window.location.reload();
                         }
                     },
-                    error: function(data){
+                    error: function(data) {
                         console.error(data);
                     }
                 })
             })
 
             /** Subscribe Newsletter**/
-            $('.newsletter-form').on('submit', function(e){
+            $('.newsletter-form').on('submit', function(e) {
                 e.preventDefault();
                 $.ajax({
                     method: 'POST',
                     url: "{{ route('subscribe-newsletter') }}",
                     data: $(this).serialize(),
-                    beforeSend: function(){
+                    beforeSend: function() {
                         $('.newsletter-button').text('loading...');
                         $('.newsletter-button').attr('disabled', true);
                     },
-                    success: function(data){
+                    success: function(data) {
+                        if (data.status === 'success') {
+                            Toast.fire({
+                                icon: 'success',
+                                title: data.message
+                            })
+                            $('.newsletter-form')[0].reset();
+                            $('.newsletter-button').text('sign up');
 
+                            $('.newsletter-button').attr('disabled', false);
+                        }
                     },
-                    error: function(data){
+                    error: function(data) {
                         $('.newsletter-button').text('sign up');
                         $('.newsletter-button').attr('disabled', false);
 
-                        if(data.status === 422){
+                        if (data.status === 422) {
                             let errors = data.responseJSON.errors;
-                            $.each(errors, function(index, value){
+                            $.each(errors, function(index, value) {
                                 Toast.fire({
                                     icon: 'error',
                                     title: value[0]
@@ -110,8 +120,6 @@
                 })
             })
         })
-
-
     </script>
 
 
