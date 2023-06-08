1c3479728c4fdc56709557793812290cb37f6eed
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-08 17:52:54 +0600

    Contact Page - Storing the Mail Data at DB

--- app/Http/Controllers/Frontend/HomeController.php
+++ app/Http/Controllers/Frontend/HomeController.php
@@ -11,6 +11,7 @@ use App\Models\Comment;
 use App\Models\Contact;
 use App\Models\HomeSectionSetting;
 use App\Models\News;
+use App\Models\RecivedMail;
 use App\Models\SocialCount;
 use App\Models\Subscriber;
 use App\Models\Tag;
@@ -285,6 +286,14 @@ class HomeController extends Controller
 
             /** Send Mail */
             Mail::to($toMail->email)->send(new ContactMail($request->subject, $request->message, $request->email));
+
+            /** store the mail */
+
+            $mail = new RecivedMail();
+            $mail->email = $request->email;
+            $mail->subject = $request->subject;
+            $mail->message = $request->message;
+            $mail->save();
 
         }catch(\Exception $e){
             toast(__($e->getMessage()));
---
+++ database/migrations/2023_06_08_112641_create_recived_mails_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recived_mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+            $table->string('subject');
+            $table->text('message');
+            $table->boolean('seen')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('recived_mails');
+    }
+};
