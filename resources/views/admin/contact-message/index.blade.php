0300225f8e09ce920f7ecd650dd649c2df4c0c72
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-10 11:00:55 +0600

    Contact Page - Working with Replay Feature (Part - 1)

--- resources/views/admin/contact-message/index.blade.php
+++ resources/views/admin/contact-message/index.blade.php
@@ -51,8 +51,7 @@
                                     @endif
                                 </td> --}}
                                 <td>
-                                    <a href=""
-                                        class="btn btn-primary"><i class="fas fa-envelope"></i></a>
+                                    <a href="" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal-{{ $message->id }}"><i class="fas fa-envelope"></i></a>
 
                                     <a href="{{ route('admin.social-link.destroy', $message->id) }}"
                                         class="btn btn-danger delete-item"><i
@@ -72,6 +71,30 @@
 
         </div>
     </section>
+
+    @foreach ($messages as $message)
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal-{{ $message->id }}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">{{ __('Replay To') }}: {{ $message->email }}</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <textarea name="repaly" class="form-control" style="height: 200px !important;"></textarea>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">{{ __('Close') }}</button>
+              <button type="button" class="btn btn-primary">{{ __('Send') }}</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    @endforeach
+
 @endsection
 
 @push('scripts')
