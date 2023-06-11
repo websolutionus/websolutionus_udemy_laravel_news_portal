02bc562a40ce51336d9e05ce7a60316bd0b5f104
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-11 16:35:42 +0600

    Appearance Settings - Working with Appearance Settings (Part - 2)

--- resources/views/frontend/layouts/master.blade.php
+++ resources/views/frontend/layouts/master.blade.php
@@ -20,6 +20,11 @@
     <link rel="icon" href="{{ asset($settings['site_favicon']) }}" type="image/png">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
     <link href="{{ asset('frontend/assets/css/styles.css') }}" rel="stylesheet">
+    <style>
+        :root {
+            --colorPrimary: {{ $settings['site_color'] }};
+        }
+    </style>
 </head>
 
 <body>
