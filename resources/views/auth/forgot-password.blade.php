ccc594ef7cf25410f1fc96b7f4fd3beec97ca833
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-19 18:25:41 +0600

    Forget Password Page Design - Adding Design To Frontend Forget Password Page

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,25 +1,40 @@
-<x-guest-layout>
-    <div class="mb-4 text-sm text-gray-600">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
-    </div>
+@extends('frontend.layouts.master')
 
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+@section('content')
+    <!-- login -->
+    <section class="wrap__section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mx-auto my-5" style="max-width: 380px;">
+                        <div class="card-body">
+                            @if (session()->has('status'))
+                                <div class="alert alert-success">{{ session('status') }}</div>
+                                
+                            @endif
+                            <h4 class="card-title mb-4">{{ __('Forget Password') }}</h4>
+                            <form method="POST" action="{{ route('password.email') }}">
+                                @csrf
 
-    <form method="POST" action="{{ route('password.email') }}">
-        @csrf
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="{{ __('frontend.Email') }}" type="text" name="email">
+                                    @error('email')
+                                        <p class="text-danger">{{ $message }}</p>  
+                                    @enderror
+                                </div>
+                              
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block"> {{ __('frontend.Email Password Reset Link') }} </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    <p class="text-center mt-4 mb-0">{{ __('frontend.Remembered your password?') }} <a href="{{ route('login') }}">{{ __('Login') }}</a></p>
+                </div>
+            </div>
         </div>
+    </section>
+    <!-- end login -->
+@endsection
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
