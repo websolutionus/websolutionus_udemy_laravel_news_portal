eabe504baa29cade82cc5ded0c6f02ce80fa7a4d
shakilahmed0369 <sakilhossain01969@gmail.com>
2023-06-19 17:21:22 +0600

    Register Page Design - Adding Design To Frontend Register Page

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,62 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('frontend.layouts.master')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('content')
+    <!-- login -->
+    <section class="wrap__section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="card mx-auto" style="max-width: 380px;">
+                        <div class="card-body">
+                            <h4 class="card-title mb-4">{{ __('Register') }}</h4>
+                            <form method="POST" action="{{ route('register') }}">
+                                @csrf
+                                {{-- <a href="#" class="btn btn-facebook btn-block mb-2 text-white"> <i
+                                        class="fa fa-facebook"></i> &nbsp; Sign
+                                    in
+                                    with
+                                    Facebook</a>
+                                <a href="#" class="btn btn-primary btn-block mb-4"> <i class="fa fa-google"></i>
+                                    &nbsp;
+                                    Sign in with
+                                    Google</a> --}}
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="{{ __('frontend.Name') }}" type="text"
+                                        name="name">
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="{{ __('frontend.Email') }}" type="email" name="email">
+                                    @error('email')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="{{ __('frontend.Password') }}" type="password"
+                                        name="password">
+                                        @error('password')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+                                <div class="form-group">
+                                    <input class="form-control" placeholder="{{ __('frontend.Confirm Password') }}" type="password"
+                                        name="password_confirmation">
+                                        @error('password_confirmation')
+                                        <p class="text-danger">{{ $message }}</p>
+                                    @enderror
+                                </div>
+
+
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block"> {{ __('frontend.Sign up') }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ml-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <!-- end login -->
+@endsection
